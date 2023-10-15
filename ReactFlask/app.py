219352f517f47276552bc97543ce5f0118ca0056
John Doe <sophia.arora.harder@gmail.com>
2023-10-15 11:08:03 -0500

    mongo working

--- ReactFlask/app.py
+++ ReactFlask/app.py
@@ -2,16 +2,38 @@ import os
 
 from flask import Flask, jsonify
 from flask_cors import cross_origin, CORS
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://sophiaharder:Patches!2017@cluster0.hsoqdrb.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.Cluster0
+userCollection = db.hardwareUser
+
+if userCollection.find_one({"username":{"$eq":"sophia"}}):
+    print("user already exists")
+else:
+    document = {"username": "sophia", "password":"harder"}
+    userCollection.insert_one(document)
+
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-users  = {
-    "sophia":"harder",
-    "sah":"patches",
-}
+#
+# users  = {
+#     "sophia":"harder",
+#     "sah":"patches",
+# }
 
 #to add a new user: user[new _username] = new_password
 
@@ -31,9 +53,9 @@ users  = {
 @cross_origin()
 def hello_world(username):
     words = username.split()
-
-    if words[0] in users:
-        if words[1] == users[words[0]]:
+    user_document = userCollection.find_one({"username": words[0]})
+    if user_document:
+        if words[1] == user_document["password"]:
             successM = {"password": "signed in", "code": 200}
             return jsonify(successM), 200
         else:
@@ -47,11 +69,13 @@ def hello_world(username):
 @cross_origin()
 def new_user(username):
     words = username.split()
-    if words[0] in users:
+    user_document = userCollection.find_one({"username": words[0]})
+    if user_document:
         errorM = {"error": "User already exists", "code": 404}
         return jsonify(errorM), 404
     else :
-        users[words[0]] = words[1]
+        document = {"username": words[0], "password": words[1]}
+        userCollection.insert_one(document)
         successM = {"result": "new user created", "code": 200}
         return jsonify(successM), 200
 
