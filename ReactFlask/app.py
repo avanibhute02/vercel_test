0c0a8a55550871c0004769bb5d8cbebf901c3e93
John Doe <sophia.arora.harder@gmail.com>
2023-10-17 13:44:40 -0500

    changed login

--- ReactFlask/app.py
+++ ReactFlask/app.py
@@ -5,7 +5,7 @@ from flask_cors import cross_origin, CORS
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://sophiaharder:Patches!2017@cluster0.hsoqdrb.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://group2:swelab@cluster0.hsoqdrb.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
